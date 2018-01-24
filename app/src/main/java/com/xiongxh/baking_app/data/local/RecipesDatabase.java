83ec245aa81184ec32db7da74f8a2a7eeffee185
xionger <xunhao.xiong@gmail.com>
2018-01-24 09:33:41 -0600

    update packages versions

--- app/src/main/java/com/xiongxh/baking_app/data/local/RecipesDatabase.java
+++ app/src/main/java/com/xiongxh/baking_app/data/local/RecipesDatabase.java
@@ -8,7 +8,7 @@ import com.xiongxh.baking_app.data.bean.Ingredient;
 import com.xiongxh.baking_app.data.bean.Recipe;
 import com.xiongxh.baking_app.data.bean.Step;
 
-@Database(entities = {Recipe.class, Ingredient.class, Step.class}, version = 1)
+@Database(entities = {Recipe.class, Ingredient.class, Step.class}, version = 5)
 public abstract class RecipesDatabase extends RoomDatabase {
     public abstract RecipesDao recipesDao();
 }
